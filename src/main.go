e18a8ea5eb0b4187747657c92e4be2d3536b2138
DiarCode <diar_beg64@mail.ru>
2023-01-24 01:28:31 +0600

    main update

--- src/main.go
+++ src/main.go
@@ -1,7 +1,5 @@
 package main
 
-//https://github.com/NikSchaefer/go-fiber
-
 import (
 	"log"
 
@@ -11,6 +9,18 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
+
+// 		POSTGRES_DSN = "host=localhost port=5432 user=postgres password=password dbname=gotodo_db sslmode=disable"
+
+// 		# Надо сделать перед запуском
+// 		# 1. Создать .env файл в корнивой директории (там где go.mod)
+// 		# 2. Засунуть туда POSTGRES_DSN, но изменить на свои парамметры (скопировать прям как сверху)
+
+// 		# Запуск проекта
+// 		# go run ./src
+
+// 		# Пример endpoint
+// 		# http://localhost:8080/api/v1/users
 
 func main() {
 	err := godotenv.Load()
