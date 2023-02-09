03a0bb5c1a993d5f23f6eb45c6f6da045706feb0
DiarCode <diar_beg64@mail.ru>
2023-02-09 15:06:51 +0600

    swap to automigrate

--- migrations/000002_create_users_table.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id bigserial PRIMARY KEY,
-    created_at timestamp(0) with time zone not null default NOW(), 
-    email text not null,
-    password text not null,
-);
