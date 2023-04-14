8c1664e2e4d966776852ef3bc8b7290bd1790b94
DiarCode <diar_beg64@mail.ru>
2023-04-14 13:21:10 +0600

    added docker compose on unit-testing

--- src/database/database.go
+++ src/database/database.go
@@ -15,10 +15,11 @@ var DB *gorm.DB
 func ConnectDB() {
 	var err error
 	dsn := fmt.Sprintf(
-		"host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai",
+		"host=db user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
 	)
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
